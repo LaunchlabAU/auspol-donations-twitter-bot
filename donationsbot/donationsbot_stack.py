71cf2b18315559edc924a7e59098a7369a192902
David Pretty <david@launchlab.com.au>
2022-02-10 19:16:41 +1100

    fix repo location

--- donationsbot/donationsbot_stack.py
+++ donationsbot/donationsbot_stack.py
@@ -27,7 +27,7 @@ class DonationsbotStack(Stack):
                     )
                 ],
                 input=pipelines.CodePipelineSource.connection(
-                    "dpretty/donationsbot",
+                    "LaunchlabAU/auspol-donations-twitter-bot",
                     "main",
                     connection_arn="arn:aws:codestar-connections:ap-southeast-2:240067924203:connection/633c3b9b-ba82-491d-9007-c3f4f159ab21",
                 ),
